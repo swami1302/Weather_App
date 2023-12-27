2def9fdd8ec1047dbfaee2ad473934387dce040b
swami1302 <swamii1413@gmail.com>
2023-12-28 02:55:44 +0530

    handled error using try and catch

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -23,29 +23,34 @@ const WeatherApp = () => {
     // console.log(element[0].value);
     if (element[0]==='') return 0;
     let URL = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${API_key}`;
+    
+    try {
 
-    let response = await fetch(URL);  //fetch the reponse data from the URL
-    let data = await response.json(); //converts response into json
-    const humidity = document.getElementsByClassName('humidity-percent');
-    const wind = document.getElementsByClassName('wind-rate');
-    const temperature = document.getElementsByClassName("weather-temp");
-    const location = document.getElementsByClassName('weather-location');
+      let response = await fetch(URL);  //fetch the reponse data from the URL
+      let data = await response.json(); //converts response into json
+      const humidity = document.getElementsByClassName('humidity-percent');
+      const wind = document.getElementsByClassName('wind-rate');
+      const temperature = document.getElementsByClassName("weather-temp");
+      const location = document.getElementsByClassName('weather-location');
 
-    humidity[0].innerHTML = data.main.humidity+" %";
-    wind[0].innerHTML = Math.floor(data.wind.speed)+" km/h";
-    temperature[0].innerHTML = Math.floor(data.main.temp)+"°C";
-    location[0].innerHTML = data.name;
+      humidity[0].innerHTML = data.main.humidity + " %";
+      wind[0].innerHTML = Math.floor(data.wind.speed) + " km/h";
+      temperature[0].innerHTML = Math.floor(data.main.temp) + "°C";
+      location[0].innerHTML = data.name;
 
-    if (data.weather[0].icon === '01d' || data.weather[0].icon === '01n') SetweatherIcon(clear_icon);
-    else if (data.weather[0].icon === '02d' || data.weather[0].icon === '02n') SetweatherIcon(cloud_icon);
-    else if (data.weather[0].icon === '03d' || data.weather[0].icon === '03n') SetweatherIcon(drizzle_icon);
-    else if (data.weather[0].icon === '04d' || data.weather[0].icon === '04n') SetweatherIcon(drizzle_icon);
-    else if (data.weather[0].icon === '09d' || data.weather[0].icon === '09n') SetweatherIcon(rain_icon);
-    else if (data.weather[0].icon === '10d' || data.weather[0].icon === '10n') SetweatherIcon(rain_icon);
-    else if (data.weather[0].icon === '13d' || data.weather[0].icon === '13n') SetweatherIcon(snow_icon);
-    else SetweatherIcon(clear_icon);
-
-
+      if (data.weather[0].icon === '01d' || data.weather[0].icon === '01n') SetweatherIcon(clear_icon);
+      else if (data.weather[0].icon === '02d' || data.weather[0].icon === '02n') SetweatherIcon(cloud_icon);
+      else if (data.weather[0].icon === '03d' || data.weather[0].icon === '03n') SetweatherIcon(drizzle_icon);
+      else if (data.weather[0].icon === '04d' || data.weather[0].icon === '04n') SetweatherIcon(drizzle_icon);
+      else if (data.weather[0].icon === '09d' || data.weather[0].icon === '09n') SetweatherIcon(rain_icon);
+      else if (data.weather[0].icon === '10d' || data.weather[0].icon === '10n') SetweatherIcon(rain_icon);
+      else if (data.weather[0].icon === '13d' || data.weather[0].icon === '13n') SetweatherIcon(snow_icon);
+      else SetweatherIcon(clear_icon);
+    }
+    catch (error) {
+      console.error('Error fetching weather data:', error);
+      alert("Invaild Location, Try again");
+    }
 
   }
 
